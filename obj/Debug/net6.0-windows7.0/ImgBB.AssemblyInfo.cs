a989a05bde541853f5c95dc2782f3a4e3f2a4d8c
Christopher <supersoftware1234@gmail.com>
2024-05-05 21:25:02 -0400

    a

--- obj/Debug/net6.0-windows7.0/ImgBB.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows7.0/ImgBB.AssemblyInfo.cs
@@ -14,10 +14,10 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ImgBB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.3")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.3+16c09c3331f826d9d1b1a2d178e3f3ecb97c5ea2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.3+d0b6ff05a8e9f593941c742a1c1372d19d3151e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ImgBB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ImgBB")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.3")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.3 ")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
 
